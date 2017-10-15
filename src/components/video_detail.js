1378bf0cc723e3c605ba14ec41a21d6d4d22ca61
Jari Rasiniemi <jari.rasiniemi@gmail.com>
2017-10-15 23:55:09 +0300

    end of section 2

---
+++ src/components/video_detail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const VideoDetail = ({video}) => {
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+    console.log(video.id.videoId);
+    const videoId = video.id.videoId;
+    // https://www.youtube.com/embed/BuF05UWgji0"
+    const url = `https://www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className="video-detail col-md-8">
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src={url}></iframe>
+          </div>
+          <div className="details">
+            <div>{video.snippet.title}</div>
+            <div>{video.snippet.description}</div>
+          </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
